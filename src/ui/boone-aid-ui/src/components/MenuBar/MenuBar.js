ec52a7e6d1ea97516cc62a9b6f1745867f6a8c05
Will White <wrwwhite@gmail.com>
2022-05-07 04:56:38 -0700

    makes menu bar work on mobile

--- src/ui/boone-aid-ui/src/components/MenuBar/MenuBar.js
+++ src/ui/boone-aid-ui/src/components/MenuBar/MenuBar.js
@@ -1,31 +1,58 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './MenuBar.css';
 import {ReactComponent as Logo} from '../../images/Logo.svg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 export default function MenuBar() {
+
+  const [mobileMenuVisible, setMobileMenuVisible] = useState(false);
+
+  function toggleMobileMenu() {
+    setMobileMenuVisible(!mobileMenuVisible);
+  }
+
+  const homeLink = (
+        <Link className="quiet-link" to='/'>
+          <div className='menu-button'>
+            Home
+          </div>
+        </Link>
+  )
+
+  const resourcesLink = (
+    <Link className="quiet-link" to='services'>
+      <div className='menu-button'>
+        Resources
+      </div>
+    </Link>
+  )
+
   return (
     <nav id="menu-bar">
       <div className='menu-section'>
-        <Link to='/'>
+        <Link className="quiet-link" to='/'>
           <div className='menu-button'>
             <span><Logo/></span>
             <span id='logo-text'>BooneAid</span>
           </div>
         </Link>
       </div>
-      <div className='menu-section'>
-        <Link to='/'>
-          <div className='menu-button'>
-            Home
-          </div>
-        </Link>
-        <Link to='services'>
-          <div className='menu-button'>
-            Resources
-          </div>
-        </Link>
+      <div id='menu-section'>
+        {homeLink}
+        {resourcesLink}
       </div>
+      <div id="daburger" onClick={() => setMobileMenuVisible(true)}>
+        <FontAwesomeIcon icon={faBars}/>
+      </div>
+      {(mobileMenuVisible) && (
+        <div id="mobile-menu">
+          <div id="exit-mobile-menu" onClick={() => setMobileMenuVisible(false)}>X</div>
+          {homeLink}
+          {resourcesLink}
+        </div>
+      )}
     </nav>
   );
 }
